682cb07d39db204c762761c581196a1f116be286
hojimurodtoshqulov <hojimurod2603@gmail.com>
2023-08-17 13:58:03 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<h1>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro,
+				expedita ad nesciunt eaque fuga tempora optio quibusdam dicta mollitia
+				adipisci? Unde, sit reiciendis nobis rem eveniet a, eius ab, cumque odit
+				sapiente accusantium quibusdam dolorum inventore expedita quis
+				assumenda. Officia illum sit eaque libero aperiam error iste itaque?
+				Possimus non blanditiis, voluptatem illum quae distinctio nulla
+				consequuntur tempore perspiciatis perferendis error quia minima. Aliquid
+				commodi voluptatum ipsum laborum, consectetur animi asperiores totam
+				nostrum numquam. Tenetur sunt laboriosam, repellendus, a vel eaque amet
+				nam pariatur ducimus minus voluptate hic officia facere non quod veniam
+				libero similique voluptas expedita nostrum architecto distinctio in
+				perspiciatis. Laudantium enim maiores dignissimos, maxime excepturi
+				corrupti, in quasi facere earum neque aliquam esse modi eveniet rerum
+				molestias perferendis soluta. Nihil quia vitae nemo quaerat commodi!
+				Necessitatibus saepe, et quasi obcaecati nulla libero voluptatum ea
+				incidunt porro explicabo tempore mollitia doloribus sed sit, dolore
+				asperiores quo exercitationem maiores maxime. Animi delectus, fugiat,
+				mollitia quo corrupti atque facilis eligendi at cumque, minus
+				exercitationem neque sint. Libero, distinctio. Harum perferendis
+				veritatis exercitationem at iusto soluta! Officia officiis blanditiis
+				nemo minus rem necessitatibus quo, culpa ea consequatur odio illum
+				quasi, veniam suscipit asperiores earum. Eligendi autem sequi
+				consectetur obcaecati est voluptatem?
+			</h1>
+		</>
+	);
 }
 
-export default App
+export default App;
