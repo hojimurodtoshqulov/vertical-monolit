d2860d2c8e68f6b7584eb08f806e2d23fa6f3894
bakhodirabdullayev <bahodirabdullayev0556@gmail.com>
2023-08-17 23:34:04 +0500

    navbar removed

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -3,45 +3,19 @@ import { Nav } from "../components/navbar/nav";
 import i18next from "i18next";
 // import Footer from "../components/footer/footer";
 // import MobileNav from "../components/mobileNav";
-import { useState, useEffect } from "react";
 
 const Layout = ({ children }) => {
   const handleClick = (e) => {
     i18next.changeLanguage(e.target.value);
   };
-  const [active, setActive] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setActive(false);
-    }, 4000);
-  }, []);
+
   return (
     <Suspense fallback="loading">
       <div className="app-wrap">
-        {active ? (
-          <>
-            {/* <video
-							src={video}
-							autoPlay
-							muted
-							playsInline
-							style={{
-								width: "100%",
-								height: "100vh",
-								position: "fixed",
-								inset: 0,
-								background: "white",
-								zIndex: 100,
-							}}
-						></video> */}
-          </>
-        ) : (
-          // <Navbar />
-          <>
-            <Nav onClick={(e) => handleClick(e)} />
-            {/* <div className="mobileNav"><MobileNav /></div> */}
-          </>
-        )}
+        <>
+          {/* <Nav onClick={(e) => handleClick(e)} /> */}
+          {/* <div className="mobileNav"><MobileNav /></div> */}
+        </>
         {children}
         {/* <Footer /> */}
       </div>
