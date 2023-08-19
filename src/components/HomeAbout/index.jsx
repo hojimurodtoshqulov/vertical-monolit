50ec10c7ffe8b0f8d83c0d233d480d041d3c8a49
bakhodirabdullayev <bahodirabdullayev0556@gmail.com>
2023-08-19 23:41:49 +0500

    home page about section

---
+++ src/components/HomeAbout/index.jsx
@@ -0,0 +1,38 @@
+import scss from "./HomeAbout.module.scss";
+import img1 from "../category/icons/home-about-1.png";
+import img2 from "../category/icons/home-about-2.png";
+import img3 from "../category/icons/home-about-3.png";
+
+const HomeAbout = () => {
+  return (
+    <>
+      {/* <div className="container"> */}
+      <div className={scss.section__wrapper}>
+        <div className={scss.section__left}>
+          <div className={scss.img}>
+            <img src={img1} alt="img1" />
+          </div>
+          <div className={scss.img}>
+            <img src={img2} alt="img2" />
+          </div>
+          <div className={scss.img}>
+            <img src={img3} alt="img3" />
+          </div>
+        </div>
+        <div className={scss.section__right}>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim omnis
+            dolorem velit sed labore veritatis.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim omnis
+            dolorem velit sed labore veritatis.
+          </p>
+        </div>
+      </div>
+      {/* </div>   */}
+    </>
+  );
+};
+
+export default HomeAbout;
