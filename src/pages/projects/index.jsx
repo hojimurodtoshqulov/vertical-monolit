58d007c8637c947982b30db28c1180051032c134
bakhodir <bahodirabdullayev0556@gmail.com>
2023-08-24 22:30:58 +0500

    projects page added

---
+++ src/pages/projects/index.jsx
@@ -0,0 +1,13 @@
+import Showcase from "../../components/showcase/showcase";
+import Workscard from "../../components/workscard";
+
+const Projects = () => {
+  return (
+    <>
+      <Showcase head1={"наши проеткы".toUpperCase()} />
+      <Workscard />
+    </>
+  );
+};
+
+export default Projects;
--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/home/home";
 import ErrorPage from "../error-page/index";
 import AboutUs from "../pages/aboutUs/aboutUs";
+import Projects from "../pages/projects";
 
 const RoutesWrapper = () => {
   return (
@@ -9,6 +10,7 @@ const RoutesWrapper = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
+        <Route path="/projects" element={<Projects />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </>
