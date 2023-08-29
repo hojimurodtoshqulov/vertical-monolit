e0ecb3828f2773e593e9124dbe541644e63880ac
bakhodir <bahodirabdullayev0556@gmail.com>
2023-08-29 17:56:20 +0500

    contact page finished

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -3,6 +3,7 @@ import Home from "../pages/home/home";
 import ErrorPage from "../error-page/index";
 import AboutUs from "../pages/aboutUs/aboutUs";
 import Projects from "../pages/projects";
+import Contact from "../pages/contact";
 
 const RoutesWrapper = () => {
   return (
@@ -11,6 +12,7 @@ const RoutesWrapper = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </>
