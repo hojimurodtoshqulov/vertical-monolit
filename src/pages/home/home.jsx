345f4c6567c0903d7b7e631f907dda18cd71564d
bakhodir <bahodirabdullayev0556@gmail.com>
2023-08-29 06:10:07 +0500

    home page category section

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,13 +4,16 @@ import Categories from "../../components/category";
 import HomeAbout from "../../components/HomeAbout";
 import Adventure from "../../components/adventure";
 import OurWorks from "../../components/our-works";
+import scss from "./home.module.scss";
 
 const Home = () => {
   return (
     <div>
       <Header head1={"Architecture"} head2={"center."} />
-      <div style={{ margin: "50px 10px" }}>
+      <div className={scss.category}>
+        {/* <div> */}
         <Categories />
+        {/* </div> */}
       </div>
       <div style={{ margin: "150px 10px" }}>
         <HomeAbout />
