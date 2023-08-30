232cb2ab51a81bac4b7f9674dacc51ee76590ba9
bakhodir <bahodirabdullayev0556@gmail.com>
2023-08-30 23:12:40 +0500

    i18n added

--- src/components/navbar/nav.jsx
+++ src/components/navbar/nav.jsx
@@ -1,11 +1,13 @@
-import { Suspense } from "react";
+import { Suspense, useRef } from "react";
 import { useState, useEffect } from "react";
 import navLogo from "../../assets/logo_head.png";
 import scss from "./nav.module.scss";
 import { Link, NavLink } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import { AiFillCaretDown } from "react-icons/ai";
+import { HiLanguage } from "react-icons/hi2";
 import { FaPhone } from "react-icons/fa";
+import "./dropdown.scss";
+const langs = ["en", "ru", "uz"];
 const Nav = () => {
   const [navColorBg, setnavColorBg] = useState(0);
   const [navSize, setnavSize] = useState("12vh");
@@ -13,6 +15,8 @@ const Nav = () => {
   const [navBg, setNavBg] = useState("transparent");
   const [navLinkColor, setNavLinkColor] = useState("");
   const [navBoxShadow, setNavBoxShadow] = useState("inset 0px 0px 0px #ff00");
+  const ref = useRef(null);
+  const [currentLang, setCurrentLang] = useState("en");
   // const [navContainerBR, setNavContainerBR] = useState("2px solid #fff");
   const listenScrollEvent = () => {
     window.scrollY > 80 ? setnavColorBg(10) : setnavColorBg(0);
@@ -34,18 +38,36 @@ const Nav = () => {
       window.removeEventListener("scroll", listenScrollEvent);
     };
   }, []);
-  const [langCheck, setLangCheck] = useState(1);
-  const [langColor, _] = useState("");
-  const currentLang = i18n.language === "en-US" ? "ru" : i18n.language;
-  const checkLangImg = (e) => {
-    if (e.target.value === "ru") {
-      i18n.changeLanguage("ru");
-      setLangCheck(2);
-    } else {
-      setLangCheck(1);
-      i18n.changeLanguage("uz");
-    }
+  // const [langCheck, setLangCheck] = useState(1);
+  // const [langColor, _] = useState("");
+  // const currentLang = i18n.language === "en-US" ? "ru" : i18n.language;
+  // const checkLangImg = (e) => {
+  //   if (e.target.value === "ru") {
+  //     i18n.changeLanguage("ru");
+  //     setLangCheck(2);
+  //   } else {
+  //     setLangCheck(1);
+  //     i18n.changeLanguage("uz");
+  //   }
+  // };
+  function myFunction() {
+    ref.current?.classList.toggle("show");
+  }
+  const langBtnClick = (lang) => {
+    i18n.changeLanguage(lang);
+    localStorage.setItem("lang", lang);
+    setCurrentLang(lang);
+    myFunction();
   };
+
+  useEffect(() => {
+    if (!localStorage.getItem("lang")) {
+      localStorage.setItem("lang", "en");
+    } else {
+      setCurrentLang(localStorage.getItem("lang"));
+      i18n.changeLanguage(localStorage.getItem("lang"));
+    }
+  }, []);
   const scrollToTop = () => {
     window.scrollTo(0, 0);
   };
@@ -113,7 +135,7 @@ const Nav = () => {
               </NavLink>
             </div>
             <div className={scss.nav__container_lang}>
-              <span
+              {/* <span
                 className={scss.nav__container_lang_ru}
                 onClick={checkLangImg}
                 value={"ru"}
@@ -141,7 +163,24 @@ const Nav = () => {
                   style={{ color: langColor }}
                 ></button>
                 <div></div>
-              </span>
+              </span> */}
+              <div className="dropdown">
+                <button className="dropbtn" onClick={() => myFunction()}>
+                  {/* <HiLanguage /> */}
+                  {currentLang}
+                </button>
+                <div id="myDropdown" ref={ref} className="dropdown-content">
+                  {langs.map((l, i) => (
+                    <span
+                      className={currentLang === l ? "active" : ""}
+                      onClick={() => langBtnClick(l)}
+                      key={i}
+                    >
+                      {l}
+                    </span>
+                  ))}
+                </div>
+              </div>
             </div>
             <div className={scss.nav__container_phoneLink}>
               <a href="tel:+998777777777">+998 77 777 77 77</a>
